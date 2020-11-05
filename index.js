24356de937eb36e9f88199077e806ea6b8dace28
krylenger <krylenger@gmail.com>
2020-11-05 14:41:27 +0800

    creating a tiny JS world

--- index.js
+++ index.js
@@ -8,6 +8,74 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+const dog = {
+    type: "animal",
+    legs: 4,
+    hands: 0,
+    name: "Sharik",
+    gender: "male",
+    friends: ["Anna", "Eugene"],
+    say: () => {
+      return "woof woof woof!";
+    },
+  },
+  cat = {
+    type: "animal",
+    legs: 4,
+    hands: 0,
+    name: "Murzik",
+    gender: "female",
+    friends: ["Anna", "Eugene"],
+    say: () => {
+      return "Mrrrrr...";
+    },
+  },
+  woman = {
+    type: "human",
+    legs: 2,
+    hands: 2,
+    name: "Anna",
+    gender: "female",
+    friends: ["Eugene", "Murzik", "Sharik"],
+    say: () => {
+      return "Nice to meet you!";
+    },
+  },
+  man = {
+    type: "human",
+    legs: 2,
+    hands: 2,
+    name: "Eugene",
+    gender: "male",
+    friends: ["Anna", "Murzik", "Sharik"],
+    say: () => {
+      return "Nice to meet you!";
+    },
+  },
+  catWoman = {
+    type: "cat-woman",
+    legs: 2,
+    hands: 2,
+    name: "Gerda",
+    gender: "female",
+    friends: ["Anna", "Murzik"],
+    say: cat.say
+  };
+
+const inhabitants = [man, woman, cat, dog, catWoman];
+
+const introduction = ({
+  type,
+  name,
+  gender,
+  legs,
+  hands,
+  friends,
+  say,
+}) => `Hi! My name is <em>${name}</em> and I'm <em>${type}</em>. 
+My gender is <em>${gender}</em>. I have <em>${legs}</em> legs and <em>${hands}</em> hands. 
+These are my friends: <em>${friends.join(", ")}</em>.<em> 
+${say()}</em><br><br>`;
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -18,14 +86,8 @@
    so code reviewers might focus on a single file that is index.js.
    */
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+//  Print examples:
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+inhabitants.forEach((inhabitant) => {
+  print(introduction(inhabitant), "pre");
+});
